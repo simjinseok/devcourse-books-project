6cb16cedb117dab632ee6a0d670897b624ba926e
Jinseok Sim <jinseok@simjinseok.com>
2024-05-20 22:30:16 +0900

    도서 목록 조회 API 추가

--- controllers/book-controller.js
+++ controllers/book-controller.js
@@ -2,7 +2,23 @@ const conn = require('../db');
 const {StatusCodes} = require("http-status-codes");
 
 function getBooks(req, res) {
+    const categoryId = Number.parseInt(req.query.category_id);
 
+    const sql = 'SELECT * FROM books WHERE category_id=$1';
+    conn.connect(() => {
+        conn.query(sql, [categoryId], (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+
+            if (result.rows.length > 0) {
+                return res.status(StatusCodes.OK).json(result.rows);
+            }
+
+            res.status(StatusCodes.NOT_FOUND).end();
+        })
+    })
 }
 
 function getBook(req, res) {
